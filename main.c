741c6af3aeec275dfb1ce3853e5b8420aa827ba9
eric <eric.thomas@formation-industries-fc.fr>
2020-11-05 15:26:13 +0100

    sprint 2 v1

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int main() {
     int nbquestions=0;
     int nombre1=0,nombre2=0;
     int operateur=0;
+    int resultat=0;
+    int resultatf1=0;
+    int resultatf2=0;
     srand(time(NULL));
     printf("A Combien de questions voulez-vous répondre?\n");
     scanf("%d",&nbquestions);
@@ -18,19 +21,22 @@ int main() {
         nombre2=(rand()%100)+1;
         operateur=(rand()%3);
 
-        /*************************génerations des questions******************************/œ
+        /*************************génerations des questions******************************/
         if(operateur==0)
         {
             printf("%d+%d\n",nombre1,nombre2);
+            resultat=nombre1+nombre2;
         } else
             if(operateur==1)
             {
                 printf("%d-%d\n",nombre1,nombre2);
+                resultat=nombre1-nombre2;
             }
             else
             {
                 printf("%d*%d\n",nombre1,nombre2);
             }
+            printf("%d\n",resultat);
     }
 
     return 0;
